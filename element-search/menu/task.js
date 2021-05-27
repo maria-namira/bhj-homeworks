f93de914976a967961fa865a7cc1dd0e21f815de
Maria <mariaes2016@yandex.ru>
2021-05-27 18:43:43 +0300

    new menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,12 +1,19 @@
+class Menu{
+	constructor(menu){
 const menu__link = document.getElementsByClassName('menu__link');
 const menu__item = document.getElementsByClassName('menu__item');
 
-for (let i = 0; i < menu__link.length; i++) {
-    menu__link.item(i).onclick = () => {
-        for (let j = 0; j < document.querySelectorAll('ul.menu_active').length; j++) {
-            document.querySelectorAll('ul.menu_active').item(j).classList.remove('menu_active');
+for (let menuSub of menu__link) {
+    menuSub.closest('.menu__sub').getElementByTagName('a')[0].onclick = (sub) => {
+        if (menuSub.classList.contains ('menu__item')) {
+            menuSub.classList.remove('menu__item')
+        }else{
+        	if (menuSub.length) menu__item[0].classList.remove('menu__item');
+                menuSub.classList.add ('menu__item');
         }
-        menu__item.item(i).querySelector('ul').classList.toggle('menu_active');  
-        return false;
-    };
-}
+         return false;
+      }
+    }
+  }
+} 
+Array.from(document.getElementsByClassName('menu_main')).forEach((menu) => new Menu(menu));
